353fcb56cade4948369debc7e34ef1fb2c77afa2
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-07-22 18:30:19 +0100

    new commit

--- lib/pages/session/SkilledPerson/monEquipe.dart
+++ lib/pages/session/SkilledPerson/monEquipe.dart
@@ -29,6 +29,46 @@ class MyTeam extends StatelessWidget {
                 )
             ),
           ),
+          SizedBox(height: 5,),
+          Container(
+            margin: const EdgeInsets.all(22),
+            height: MediaQuery.of(context).size.height*0.2,
+            child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: const BorderSide(color: Colors.white)),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const MyTeam()));
+                },
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.white,
+                textColor: Colors.white,
+                child: const ListTile(
+                  leading:Text("Projet -Nom du projet"),
+                  trailing: Icon(Icons.arrow_forward_sharp),
+                )
+            ),
+          ),
+          SizedBox(height: 5,),
+          Container(
+            margin: const EdgeInsets.all(22),
+            height: MediaQuery.of(context).size.height*0.2,
+            child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: const BorderSide(color: Colors.white)),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const MyTeam()));
+                },
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.white,
+                textColor: Colors.white,
+                child: const ListTile(
+                  leading:Text("Membre de l'equipe"),
+                  trailing: Icon(Icons.arrow_forward_sharp),
+                )
+            ),
+          ),
         ],
       )
     );
